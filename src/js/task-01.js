d141945ea1a318f0a55a6a717ec9d7aac499beb3
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-11-05 10:47:31 +0200

    task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,29 @@
-console.log(document.querySelectorAll(`.item`));
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+const catCount = document.querySelectorAll(`li.item`).length;
+console.log(`Number of categories:`,catCount,`
+`);
+
+
+
+const catItem = document.querySelectorAll(`.item`);
+    catItem.forEach(item => {
+    console.log(`Category:`, item.firstElementChild.textContent);
+    console.log(`Elements:`, item.lastElementChild.children.length;
+});
